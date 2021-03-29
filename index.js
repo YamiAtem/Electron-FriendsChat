a3c3373cc73f3951d1ce7b9dd58331428626e97e
Atharva Mishra <atharvamishra956@gmail.com>
2021-03-29 21:56:48 +0530

    created app

---
+++ index.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      preload: path.join(__dirname, 'preload.js'),
+      nativeWindowOpen: true
+    }
+  });
+
+  mainWindow.loadURL("https://yamiatem.github.io/React-FriendsChat/");
+  mainWindow.setMenu(null);
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
